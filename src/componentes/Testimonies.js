ec1ecd25e4d525c37f07bc679f32413dd5afda02
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2022-06-30 18:41:48 -0500

    [ADDED] Changed functional components to class components

--- src/componentes/Testimonies.js
+++ src/componentes/Testimonies.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import '../stylesheets/Testimony.css'
 
-function Testimony(props){
+class Testimony extends React.Component {
+  render() {
+
     return(
       <div className='testimony-container'>
-        <img className='testimony-imagen'
-        src={require(`../images/testimony-${props.image}.jpeg`)}
-        alt='Foto de Christina' />
+        <img 
+        className='testimony-imagen'
+        src={require(`../images/testimony-${this.props.image}.jpeg`)}
+        alt={`Foto de ${this.props.name}` } />
         <div className='testimony-text-container'>
             <p className='testimony-name'>
-              <strong>{props.name} </strong>en <strong>{props.country
+              <strong>{this.props.name} </strong>en <strong>{this.props.country
               }</strong></p>
-            <p className='job-testimony'>{props.job} en <strong>{props.company}</strong></p>
-            <p className='text-testimony'>"{props.testimony}"</p>
+            <p className='job-testimony'>{this.props.job} en <strong>{this.props.company}</strong></p>
+            <p className='text-testimony'>"{this.props.testimony}"</p>
         </div>
       </div>
     );
+
+
+  }
 }
+
+// function Testimony(props){
+//     return(
+//       <div className='testimony-container'>
+//         <img className='testimony-imagen'
+//         src={require(`../images/testimony-${props.image}.jpeg`)}
+//         alt='Foto de Christina' />
+//         <div className='testimony-text-container'>
+//             <p className='testimony-name'>
+//               <strong>{props.name} </strong>en <strong>{props.country
+//               }</strong></p>
+//             <p className='job-testimony'>{props.job} en <strong>{props.company}</strong></p>
+//             <p className='text-testimony'>"{props.testimony}"</p>
+//         </div>
+//       </div>
+//     );
+// }
 export default Testimony;
